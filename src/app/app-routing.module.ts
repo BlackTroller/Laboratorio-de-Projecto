9dff06333f910c9afb9714f59bbdc1041fead51e
Pedro_Ribeiro_ESTG <pedro.miguel.ribeiro01@gmail.com>
2021-11-22 11:46:52 +0000

    Routing Finalizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+import { DecksComponent } from './decks/decks.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { CardsComponent } from './cards/cards.component';
+import { ExercisesComponent } from './exercises/exercises.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'users', component: UsersComponent},
+  {path: 'decks', component: DecksComponent},
+  {path: 'cards', component: CardsComponent},
+  {path: 'exercises', component: ExercisesComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: '/home', pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { DecksComponent } from './decks/decks.component';
+import { CardsComponent } from './cards/cards.component';
+import { ExercisesComponent } from './exercises/exercises.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    DecksComponent,
+    CardsComponent,
+    ExercisesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
